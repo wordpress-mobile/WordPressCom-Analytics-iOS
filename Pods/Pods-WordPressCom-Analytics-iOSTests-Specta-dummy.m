742c43a5964a6eaae48c2600eb9a6d96e71ce233
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-07-25 23:32:13 -0700

    Removing Pods

--- Pods/Pods-WordPressCom-Analytics-iOSTests-Specta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WordPressCom_Analytics_iOSTests_Specta : NSObject
-@end
-@implementation PodsDummy_Pods_WordPressCom_Analytics_iOSTests_Specta
-@end
