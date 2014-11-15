7183661f408ed7bb36c82870e2da982d027f12c0
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-11-14 16:11:09 -0800

    Fixing typo

--- WordPressCom-Analytics-iOS/WPAnalytics.h
+++ WordPressCom-Analytics-iOS/WPAnalytics.h
@@ -3,7 +3,7 @@
 typedef NS_ENUM(NSUInteger, WPAnalyticsStat) {
     WPAnalyticsStatNoStat, // Since we can't have a nil enum we'll use this to act as the nil
     WPAnalyticsStatAddedSelfHostedSite,
-    WPANalyticsStatAppReviewsCanceledFeedbackScreen,
+    WPAnalyticsStatAppReviewsCanceledFeedbackScreen,
     WPAnalyticsStatAddedSelfHostedSiteButJetpackNotConnectedToWPCom,
     WPAnalyticsStatAppReviewsDeclinedToRateApp,
     WPAnalyticsStatAppReviewsDidntLikeApp,
