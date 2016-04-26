4995f5d67cd2b72ac8e2cbdfc570eddbdf3afd1f
aerych <aerychj@gmail.com>
2016-04-26 17:46:58 -0500

    Adds enum for ab tests. Bumps podspec.

--- WordPressCom-Analytics-iOS/WPAnalytics.h
+++ WordPressCom-Analytics-iOS/WPAnalytics.h
@@ -2,6 +2,7 @@
 
 typedef NS_ENUM(NSUInteger, WPAnalyticsStat) {
     WPAnalyticsStatNoStat, // Since we can't have a nil enum we'll use this to act as the nil
+    WPAnalyticsStatABTestStart,
     WPAnalyticsStatAddedSelfHostedSite,
     WPAnalyticsStatAddedSelfHostedSiteButJetpackNotConnectedToWPCom,
     WPAnalyticsStatAppInstalled,
