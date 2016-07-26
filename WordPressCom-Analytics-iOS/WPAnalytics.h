7da5ffb8a1b253d666535d73f7dee9da329eff46
Will Kwon <will@kwon.co>
2016-07-25 22:18:49 -0500

    Adding created site analytics

--- WordPressCom-Analytics-iOS/WPAnalytics.h
+++ WordPressCom-Analytics-iOS/WPAnalytics.h
@@ -18,6 +18,7 @@ typedef NS_ENUM(NSUInteger, WPAnalyticsStat) {
     WPAnalyticsStatApplicationClosed,
     WPAnalyticsStatApplicationOpened,
     WPAnalyticsStatCreatedAccount,
+    WPAnalyticsStatCreatedSite,
     WPAnalyticsStatDefaultAccountChanged,
     WPAnalyticsStatEditorAddedPhotoViaLocalLibrary,
     WPAnalyticsStatEditorAddedVideoViaLocalLibrary,
